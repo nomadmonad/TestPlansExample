dc224b5efa5caba892e4161cc2bf1c23379513d9
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jp.ricoh.com>
2020-03-05 16:19:23 +0900

    initial commit

---
+++ TestPlansExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPlansExample
+//
+//  Created by Tsuyoshi Yamamoto on 2020/03/05.
+//  Copyright © 2020 Tsuyoshi Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
